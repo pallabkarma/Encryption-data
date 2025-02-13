208f108141584ef32696732f8f034ab69a30db29
Pallab karmakar <pllb.krmkr@gmail.com>
2025-02-13 09:09:39 +0530

    Add files via upload

---
+++ Encryption output.py
@@ -0,0 +1,8 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: C:\Users\l\Downloads\stego.py ====================
+Enter a secret message:hello
+Enter a passcode:1234
+Enter passcode for Decryption1234
+Decryption message: hello
